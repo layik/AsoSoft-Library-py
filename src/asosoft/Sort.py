9447869820ba5e2c0ff8499c4fa8da023d693ae2
Layik Hama <layik.hama@gmail.com>
2024-04-07 19:18:29 +0100

    src/asosoft/Sort.py: change sort func

--- src/asosoft/Sort.py
+++ src/asosoft/Sort.py
@@ -1,15 +1,21 @@
 def kurdish_sort(inputList):
-    ku = list(["ئءاآأإبپتثجچحخدڎڊذرڕزژسشصضطظعغفڤقكکگڴلڵمنوۆۊۉۋهھەیێ"])
+    # Kurdish alphabet in correct order
+    ku = ["ئ", "ء", "ا", "آ", "أ", "إ", "ب", "پ", "ت", "ث",
+          "ج", "چ", "ح", "خ", "د", "ڎ", "ڊ", "ذ", "ر", "ڕ",
+          "ز", "ژ", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ",
+          "ف", "ڤ", "ق", "ك", "ک", "گ", "ڴ", "ل", "ڵ",
+          "م", "ن", "و", "ۆ", "ۊ", "ۉ", "ۋ",
+          "ه", "ھ", "ە", "ی", "ێ"]
     return CustomSort(inputList, ku)
 
+
 def CustomSort(inputList, inputOrder):
-    base_char = 62000
-    order = [chr(base_char + i) for i in range(len(inputOrder))]
-    for i in range(len(inputList)):
-        for j in range(len(order)):
-            inputList[i] = inputList[i].replace(inputOrder[j], order[j])
-    inputList.sort()
-    for i in range(len(inputList)):
-        for j in range(len(order)):
-            inputList[i] = inputList[i].replace(order[j], inputOrder[j])
-    return inputList
+    extended_ku = inputOrder + [chr(i) for i in range(128)]
+    order_map = {ch: i for i, ch in enumerate(extended_ku)}
+
+    def sort_key(s):
+        # Convert each string to a list of its character order values,
+        # with a fallback value that places unknown characters at the end.
+        return [order_map.get(ch, len(extended_ku)) for ch in s]
+
+    return sorted(inputList, key=sort_key)
