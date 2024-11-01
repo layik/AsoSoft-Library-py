b021a511a9c5db9f3f6e62414601dc584ab7f2ef
Layik Hama <layik.hama@gmail.com>
2024-11-01 10:49:54 +0000

    La2Ar dr feryad

--- src/asosoft/Transliteration.py
+++ src/asosoft/Transliteration.py
@@ -1,9 +1,9 @@
 import re
 import os
 from .G2P import KurdishG2P
-_latinLetters = "a-zêîûçşéúıŕřĺɫƚḧẍḍṿʔ"
+latin_letters = "a-zêîûçşéúıŕřĺɫƚḧẍḍṿʔ"
 
-_TransliterationReplaces = {
+transliteration_replaces = {
     "LaDi2Ar": [
         "gh", "ẍ",
         "hh", "ḧ",
@@ -15,9 +15,12 @@ _TransliterationReplaces = {
         "\u201D", "»",
         rf"([0-9])([\'’-])([aeiouêîûéú])", r"\1\3",     # (e.g. 1990'an 5'ê)
         "ʔ", "",    # glottal stop
-        rf"(^|[^{_latinLetters}0-9\"’])([aeiouêîûéú])", r"\1ئ\2", #insert initial hamza
+        # there is an issue here it should
+        # convert ew to ئەو
+        rf"(^|\s)([aeiouêîûéú])", r"\1ئ\2",  # insert initial hamza at start or after a space        # this does not do the job
+        # rf"(^|[^{latin_letters}0-9\"’])([aeiouêîûéú])", r"\1ئ\2", #insert initial hamza
         rf"([aeouêîûéú])([aeiouêîûéú])", r"\1ئ\2",     #insert hamza between adjacent vowels
-        rf"(ئ)([uû])([^{_latinLetters}0-9])", r"و\3",     #omit the inserted hamza for "û" (=and)
+        rf"(ئ)([uû])([^{latin_letters}0-9])", r"و\3",     #omit the inserted hamza for "û" (=and)
         "a", "ا",
         "b", "ب",
         "ç", "چ",
@@ -29,13 +32,13 @@ _TransliterationReplaces = {
         "f", "ف",
         "g", "گ",
         "h", "ه",
-        "ḧ", "ح",
+        "ḧ|ẖ", "ح",
         "i|ı", "",
         "î|y|í", "ی",
         "j", "ژ",
         "k", "ک",
         "l", "ل",
-        "ɫ|ł|ƚ|Ɨ|ĺ", "ڵ",
+        "ɫ|ł|ƚ|Ɨ|ĺ|ḻ", "ڵ",
         "m", "م",
         "n", "ن",
         "ŋ", "نگ",
@@ -44,7 +47,7 @@ _TransliterationReplaces = {
         "p", "پ",
         "q", "ق",
         "r", "ر",
-        "ř|ŕ", "ڕ",
+        "ř|ŕ|ṟ", "ڕ",
         "s", "س",
         "ş|š|ș|s̩", "ش",
         "ṣ", "ص",
@@ -59,26 +62,27 @@ _TransliterationReplaces = {
         "z", "ز",
         rf"ه($|[^ابپتجچحخدرڕزژسشصعغفڤقکگلڵمنوۆهەیێ])", r"هـ\1",  # word-final h
         "\"|’", "ئ", # need checking, not sure "ع" or "ئ"
+        "ʿ", "ع",
         "\\u003F", "؟", #question mark
         ",", "،",  #comma
         ";", "؛",  #semicolon
     ]
 }
 
-def _replaceByList(text, replaceList):
+def replace_by_list(text, replaceList):
     for i in range(0, len(replaceList), 2):
         text = re.sub(replaceList[i], replaceList[i + 1], text)
     return text
 
 # Transliterating the Latin script into Arabic script of Kurdish (e.g. çak→چاک)
 def La2Ar(text):
-    text = _replaceByList(text.lower(), _TransliterationReplaces["La2Ar"])
+    text = replace_by_list(text.lower(), transliteration_replaces["La2Ar"])
     return text
 
 def LaDigraph2Ar(text):
     text = text.lower()
-    text = _replaceByList(text, _TransliterationReplaces["LaDi2Ar"])
-    text = _replaceByList(text, _TransliterationReplaces["La2Ar"])
+    text = replace_by_list(text, transliteration_replaces["LaDi2Ar"])
+    text = replace_by_list(text, transliteration_replaces["La2Ar"])
     return text
 
 #Transliterating the Arabic script into Latin script of Kurdish (e.g. چاک→çak)
@@ -95,7 +99,7 @@ def Ar2LaSimple(text):
     return text
 
 # Transliterating the Arabic script into Latin script of Kurdish (e.g. چاک→çak)
-def Ar2LaF(text):
+def Ar2LaFeryad(text):
     text = Phonemes2Hawar(KurdishG2P(text, backMergeConjunction=False))
     text = text.replace("ˈ", "")
     text = text.replace("ř", "ṟ")
@@ -106,17 +110,18 @@ def Ar2LaF(text):
     text = text.replace("ʔ", "")
     return text
 
-_path = os.path.dirname(__file__) 
+path = os.path.dirname(__file__)
 # Converts the output of the G2P into IPA (e.g. ˈdeˈçê→da.t͡ʃɛ)
 def Phonemes2IPA(text):
-    text = re.sub(r'(?<=(^|\W))ˈ', '', text)
+    text = re.sub(r'^ˈ', '', text)
+    text = re.sub(r'(?<=\W)ˈ', '', text)
     text = re.sub(r'ˈ', '·', text) #middle dot
-    
-    with open(os.path.join(_path, "resources/Phoneme2IPA.csv"), 'r') as file:
+
+    with open(os.path.join(path, "resources/Phoneme2IPA.csv"), 'r', encoding="utf-8") as file:
         Phoneme2IPA = file.readlines()
 
     for i in range(1, len(Phoneme2IPA)):
-        item = Phoneme2IPA[i].split(',')
+        item = Phoneme2IPA[i].strip().split(',')
         text = re.sub(item[0], item[1], text)
     return text
 
@@ -130,11 +135,11 @@ def Phonemes2Hawar(text):
 # Converts the output of the G2P into Jira's ASCII format (e.g. ˈdeˈçim→D▪A▪CH▪M)
 def Phonemes2ASCII(text):
     text = re.sub(r'[iˈ]', '', text)
-    
-    with open(os.path.join(_path, "resources/Phoneme2Ascii.csv"), 'r') as file:
+
+    with open(os.path.join(path, "resources/Phoneme2Ascii.csv"), 'r', encoding="utf-8") as file:
         Phoneme2Ascii = file.readlines()
 
     for i in range(1, len(Phoneme2Ascii)):
-        item = Phoneme2Ascii[i].split(',')
+        item = Phoneme2Ascii[i].strip().split(',')
         text = re.sub(item[0], item[1] + '▪', text)
     return text
